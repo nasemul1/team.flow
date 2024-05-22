95f8bb5a437cb8c9ac1cd996fda816c786f56000
Md Nasemul Islam <83567564+nasemul1@users.noreply.github.com>
2024-05-22 09:01:46 +0600

    modified page2

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+"use client"
 import Navbar from '@/components/navbar/navbar';
 import "./page.css";
 import graphPic from "../public/graph.png";
@@ -8,8 +9,15 @@ import hubspot from "../public/logo/hubspot.png";
 import slack from "../public/logo/slack.png";
 import screenShot1 from "../public/chat.png";
 import Image from 'next/image';
+import { useState } from 'react';
 
 const page = () => {
+    const [toggleState, setToggleState] = useState(1);
+
+    function toggleTap(index){
+        setToggleState(index);
+    }
+
     return (
         <>
             <Navbar />
@@ -40,19 +48,31 @@ const page = () => {
             <div id="page2">
                 <Image src={screenShot1} alt="App screenshot"></Image>
                 <div id="features">
-                    <div className="feature">
+                    <div 
+                        className={toggleState === 1 ? "feature active":"feature"}
+                        onClick={() => toggleTap(1)}
+                    >
                         <h3>Survey your team</h3>
                         <p>Powerful questions that get to the heart of how team members really feel.</p>
                     </div>
-                    <div className="feature">
+                    <div
+                        className={toggleState === 2 ? "feature active":"feature"}
+                        onClick={() => toggleTap(2)} 
+                    >
                         <h3>Resolve issues quickly</h3>
                         <p>Anonymous messaging that connects managers and employees.</p>
                     </div>
-                    <div className="feature">
+                    <div 
+                        className={toggleState === 3 ? "feature active":"feature"}
+                        onClick={() => toggleTap(3)}
+                    >
                         <h3>Plan your 1-on-1s</h3>
                         <p>Plan meetings together and give a stake employees and teams.</p>
                     </div>
-                    <div className="feature">
+                    <div 
+                        className={toggleState === 4 ? "feature active":"feature"}
+                        onClick={() => toggleTap(4)}
+                    >
                         <h3>Track your progress</h3>
                         <p>Easy-to-read reports and sharable results help managers and teams.</p>
                     </div>
